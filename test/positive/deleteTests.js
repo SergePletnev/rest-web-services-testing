ba0dab65a3d66ddfc812f504604a52acc48eeee4
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-08-14 01:00:51 +0300

    dev put tests

--- test/positive/deleteTests.js
+++ test/positive/deleteTests.js
@@ -1,27 +1,27 @@
-const { expect } = require('chai');
-const sendRequest = require('./../../util/sendRequest');
-const deleteRequests = require('./../../data/requests/positive/delete.requests');
-const env = require('./../../endpoint/test');
+// const { expect } = require('chai');
+// const sendRequest = require('./../../util/sendRequest');
+// const deleteRequests = require('./../../data/requests/positive/delete.requests');
+// const env = require('./../../endpoint/test');
 
-describe('[DELETE] Positive requests tests', () => {
+// describe('[DELETE] Positive requests tests', () => {
 
-    const emptyBody = {};
-    deleteRequests.forEach((request) => {
-        let response;
-        let url = request.uri;
+//     const emptyBody = {};
+//     deleteRequests.forEach((request) => {
+//         let response;
+//         let url = request.uri;
 
-        before(async () => {
-            request.uri = env.uri + url;
-            response = await sendRequest(request);
-        });
+//         before(async () => {
+//             request.uri = env.uri + url;
+//             response = await sendRequest(request);
+//         });
 
-        it(`[DELETE ${url}]: verifying response status code`, () => {
-            expect(response.statusCode).to.eql(request.expectedStatusCode);
-        });
+//         it(`[DELETE ${url}]: verifying response status code`, () => {
+//             expect(response.statusCode).to.eql(request.expectedStatusCode);
+//         });
 
-        it(`[DELETE ${url}]: verifying response body`, () => {
-            expect(response.body).to.eql(emptyBody);
-        });
-    });
+//         it(`[DELETE ${url}]: verifying response body`, () => {
+//             expect(response.body).to.eql(emptyBody);
+//         });
+//     });
 
-});
+// });
