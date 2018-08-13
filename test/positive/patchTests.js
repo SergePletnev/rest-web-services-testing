5968028e86f206ac8b948117f335432bb607b934
Siarhei Plistniou <siarhei_pliatniou@epam.com>
2018-08-14 00:04:45 +0300

    dev positive tests

---
+++ test/positive/patchTests.js
@@ -0,0 +1,31 @@
+// const { expect } = require('chai');
+// const sendRequest = require('./../../util/sendRequest');
+// const patchRequests = require('./../../data/requests/positive/patch.requests');
+// const env = require('./../../endpoint/test');
+
+// describe('[PATCH] Positive requests tests', () => {
+
+//     patchRequests.forEach((request) => {
+//         let response;
+//         let url = request.uri;
+//         let id = parseInt(url.split('/')[2], 10);
+
+//         before(async () => {
+//             request.uri = env.uri + url;
+//             response = await sendRequest(request);
+//         });
+
+//         it(`[PATCH ${url}]: verifying response status code`, () => {
+//             expect(response.statusCode).to.eql(request.expectedStatusCode);
+//         });
+
+//         it(`[PATCH ${url}]: verifying response id`, () => {
+//             expect(response.body.id).to.eql(id);
+//         });
+
+//         it(`[PATCH ${url}]: comparing sent and received data`, () => {
+//             expect(response.body).to.includes(request.body);
+//         });
+//     });
+
+// });
---
+++ test/positive/postTests.js
@@ -0,0 +1,32 @@
+// const { expect } = require('chai');
+// const sendRequest = require('./../../util/sendRequest');
+// const postRequests = require('./../../data/requests/positive/post.requests');
+// const env = require('./../../endpoint/test');
+// const getValidationErrors = require('./../../util/helper');
+
+// describe('[POST] Positive requests tests', () => {
+
+//     postRequests.forEach((request) => {
+//         let response;
+//         let url = request.uri;
+
+//         before(async () => {
+//             request.uri = env.uri + url;
+//             response = await sendRequest(request);
+//         });
+
+//         it(`[POST ${url}]: verifying response status code`, () => {
+//             expect(response.statusCode).to.eql(request.expectedStatusCode);
+//         });
+
+//         it(`[POST ${url}]: verifying response body`, () => {
+//             const errors = getValidationErrors(response.body, request.responseSchema);
+//             expect(errors.length).to.equal(0);
+//         });
+
+//         it(`[POST ${url}]: comparing sent and received data`, () => {
+//             expect(response.body).to.eql(request.body);
+//         });
+//     });
+
+// });
